ff20f19c895d542d2016d52e729f2a98af035e46
Zeyn28 <zeynepaygun06@gmail.com>
2023-12-14 20:16:59 +0300

    ilkpush

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <router-view>
+  <SignUp />
+ </router-view>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './views/HomeView.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUp
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -21,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { VueResponsiveness } from 'vue-responsiveness'
 
-createApp(App).mount('#app')
+
+createApp(App).use(router).mount('#app').use(VueResponsiveness)
